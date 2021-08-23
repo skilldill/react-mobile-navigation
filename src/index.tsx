b3b2dbf6442fe4131f51be1315ee9448d9cf9492
Александр Утбанов <utbanovag@it2g.ru>
2021-08-23 23:57:53 +0300

    add export

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export {MobileNavigation, useMobileNavigation} from "./MobileNavigation";
+export {Stack, StackScreen} from "./StackNavigation";

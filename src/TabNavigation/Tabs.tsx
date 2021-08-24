9b498c4c4f5630b4e9b57b75f90d6ddec0176e64
Александр Утбанов <utbanovag@it2g.ru>
2021-08-24 14:20:20 +0300

    add action stylied for tabbar

--- src/TabNavigation/Tabs.tsx
+++ src/TabNavigation/Tabs.tsx
@@ -1,13 +1,17 @@
-import React, { FC, useState } from "react";
+import React, { CSSProperties, FC, useState } from "react";
+import cn from "classnames";
 
 import styles from "./TabNavigation.module.css";
 
 interface TabsProps {
     onChange?: (tabName: string) => void;
+
+    classTabbar?: string,
+    styleTabbar?: CSSProperties,
 }
 
 export const Tabs: FC<TabsProps> = (props) => {
-    const {children, onChange} = props;
+    const {children, onChange, classTabbar, styleTabbar} = props;
 
     const [active, setActive] = useState(0);
 
@@ -16,10 +20,12 @@ export const Tabs: FC<TabsProps> = (props) => {
         !!onChange && onChange(tabName);
     }
 
+    const classesTabbar = !!classTabbar ? cn([styles.tabbar, classTabbar]) : styles.tabbar;
+
     return (
         <div className={styles.tabs}>
             {(children as any[])[active]}
-            <div className={styles.tabbar}>
+            <div className={classesTabbar} style={styleTabbar}>
                 <div className={styles.tabbarContent}>
                     {(children as any[]).map((tab, i) => 
                         <div key={i} onClick={() => handleChangeTab(i, tab.props.name)}>
